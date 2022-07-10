b7eeadd3ac1141cebb10cf3a833ee76dcdb3f983
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-10 19:15:16 -0300

    navegação para páginas anteriores

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,22 @@
+import { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+import Submit from "../components/Submit";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Fragment>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/login">LOGOUT</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">ABOUT</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </Fragment>
+  );
 }
 
 export default Home;

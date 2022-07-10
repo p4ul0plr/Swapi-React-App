8d3ec044589fd8419b714b9d4d2acb7e1adde9cd
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-10 15:59:27 -0300

    componente iput

---
+++ src/components/Input.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { colors } from "../styles/themes/colors";
+import { sizes } from "../styles/themes/sizes";
+
+interface InputProps {
+  $placeholder?: string;
+  $type?: string;
+}
+
+const Input = styled.input.attrs((props: InputProps) => ({
+  placeholder: props.$placeholder,
+  type: props.$type,
+}))<InputProps>`
+  background-color: ${colors.INPUT_BACKGROUND};
+  border-radius: 3px;
+  width: 100%;
+  font-size: ${sizes.XS};
+  color: ${colors.TITLE_2};
+  padding: 5px 10px;
+  line-height: 30px;
+  border: 1px solid ${colors.BORDER};
+  box-shadow: none;
+
+  &:focus-visible {
+    background-color: ${colors.TERCIARY};
+    border-color: ${colors.WHITE};
+    color: ${colors.WHITE};
+  }
+`;
+
+export default Input;

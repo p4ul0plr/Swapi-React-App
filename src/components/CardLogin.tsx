fd59955d1322d2c12042308bff75062bf7489e11
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-10 15:58:53 -0300

    componente cardLogin

---
+++ src/components/CardLogin.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { colors } from "../styles/themes/colors";
+
+const CardLogin = styled.div`
+  background-color: ${colors.SECONDARY};
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.3);
+  width: auto;
+  height: auto;
+  padding: 45px 40px;
+`;
+
+export default CardLogin;

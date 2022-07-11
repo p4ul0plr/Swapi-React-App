3f1299dd56cab27864f435bb800141c2e94cf8c3
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-11 20:02:23 -0300

    limpando página de único filme

--- src/pages/Film.tsx
+++ src/pages/Film.tsx
@@ -1,34 +1,9 @@
 import { useParams } from "react-router-dom";
-import { BASE_URL } from "../api";
-import { useFatch } from "../hooks/useFetch";
-import { FilmResponse, MovieDetailType } from "../interfaces/api";
 
 function Film() {
   const { id } = useParams<{ id: string }>();
-  const { data: film } = useFatch<MovieDetailType>(BASE_URL + id);
 
-  return (
-    <ul>
-      <li>
-        <span>{film?.title}</span>
-      </li>
-      {/* <li>
-        <span>{film?.release_date}</span>
-      </li> */}
-      <li>
-        <p>{film?.description}</p>
-      </li>
-      <li>
-        <p>{film?.director}</p>
-      </li>
-      <li>
-        <p>{film?.producer}</p>
-      </li>
-      <li>
-        <p>{film?.characters}</p>
-      </li>
-    </ul>
-  );
+  return <h1>{id}</h1>;
 }
 
 export default Film;

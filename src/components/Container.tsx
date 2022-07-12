e553c2244419ee5e7e5c2a6afc76bd263097b25b
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-12 15:31:03 -0300

    componente container

---
+++ src/components/Container.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  @media (max-width: 575px) {
+    padding: 5px;
+  }
+
+  @media (min-width: 576px) {
+    padding: 10px;
+  }
+
+  @media (min-width: 768px) {
+    padding: 10px;
+  }
+
+  @media (min-width: 992px) {
+    padding: 20px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 20px 0;
+    max-width: 1170px;
+    margin: 0 auto;
+  }
+`;

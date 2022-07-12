8098c43b219e7626ec370e86d9aed289002b1e07
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-12 15:30:50 -0300

    componente body

---
+++ src/components/Body.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import galaxyBackground from "../assets/images/backcgrounds/galaxybackground.png";
+
+export const Body = styled.div`
+  background-image: url(${galaxyBackground});
+  background-size: contain;
+  background-position: center;
+  background-repeat: repeat;
+`;

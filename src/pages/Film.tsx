8ac462779b6d5cc6db44e6db360734602720e35c
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-12 20:00:34 -0300

    adicionado esqueleto da página

--- src/pages/Film.tsx
+++ src/pages/Film.tsx
@@ -1,9 +1,22 @@
+import { Fragment } from "react";
 import { useParams } from "react-router-dom";
+import Body from "../components/Body";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { FILM_MOCK_DATA } from "../constants";
 
 function Film() {
   const { id } = useParams<{ id: string }>();
 
-  return <h1>{id}</h1>;
+  return (
+    <Fragment>
+      <Header />
+      <Body>
+        <h1>{id}</h1>
+      </Body>
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default Film;

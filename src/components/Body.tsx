c31c716b3a2fe7a20e804a58507e3faa53d083eb
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-12 19:14:41 -0300

    refatoração do componente body

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,9 +1,51 @@
+import { JSXElementConstructor } from "react";
 import styled from "styled-components";
 import galaxyBackground from "../assets/images/backcgrounds/galaxybackground.png";
 
-export const Body = styled.div`
+const BodyStyle = styled.div`
   background-image: url(${galaxyBackground});
   background-size: contain;
   background-position: center;
   background-repeat: repeat;
 `;
+
+const ContainerBodyStyled = styled.div`
+  @media (max-width: 575px) {
+    padding: 5px;
+  }
+
+  @media (min-width: 576px) {
+    padding: 10px;
+  }
+
+  @media (min-width: 768px) {
+    padding: 10px;
+  }
+
+  @media (min-width: 992px) {
+    padding: 20px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 20px 0;
+    max-width: 1170px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    min-height: calc(100vh - 117px - 104.05px);
+  }
+`;
+
+interface BodyProps {
+  children?: JSX.Element | JSX.Element[];
+}
+
+function Body({ children }: BodyProps) {
+  return (
+    <BodyStyle>
+      <ContainerBodyStyled>{children}</ContainerBodyStyled>
+    </BodyStyle>
+  );
+}
+
+export default Body;

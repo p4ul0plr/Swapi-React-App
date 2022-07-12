7fb443ffb20e51401cd010f11d8d723c637391ff
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-12 19:27:20 -0300

    ajustado a responsividade do grid

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,13 +1,32 @@
 import styled from "styled-components";
 
 export const Grid = styled.ul`
+  @media (max-width: 575px) {
+    grid-template-columns: 1fr;
+    display: grid;
+    grid-gap: 5px;
+  }
+
+  @media (min-width: 576px) {
+    grid-template-columns: 1fr;
+    display: grid;
+    grid-gap: 10px;
+  }
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr;
+    display: grid;
+    grid-gap: 10px;
+  }
+
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
     display: grid;
     grid-gap: 20px;
   }
-  @media (max-width: 992px) {
-    grid-template-columns: 1fr;
+
+  @media (min-width: 1200px) {
+    grid-template-columns: 1fr 1fr;
     display: grid;
     grid-gap: 20px;
   }

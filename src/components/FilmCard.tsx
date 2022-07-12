26c6e117c0a4285b5a64d316f45e660dce9246ce
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-12 16:18:03 -0300

    alteração no filmCard

--- src/components/FilmCard.tsx
+++ src/components/FilmCard.tsx
@@ -1,4 +1,6 @@
 import { Column } from "./Column";
+import { colors } from "../styles/themes/colors";
+import { sizes } from "../styles/themes/sizes";
 import { Link } from "react-router-dom";
 import { Row } from "./Row";
 import styled from "styled-components";
@@ -13,19 +15,10 @@ interface ImageProps {
   $backgroundImage: string | undefined;
 }
 
-const Image = styled.div<ImageProps>`
-  background-image: url(${({ $backgroundImage }) => $backgroundImage});
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: inherit;
-  height: 180px;
-`;
-
 interface FilmCardProps {
   $id: number;
   $title: string;
-  $descriptiom: string;
+  $description: string;
 }
 
 const getFilmImage = (id: number) => {
@@ -45,20 +38,101 @@ const getFilmImage = (id: number) => {
   }
 };
 
-function FilmCard({ $id, $title, $descriptiom }: FilmCardProps) {
+const Image = styled.div<ImageProps>`
+  background-image: url(${({ $backgroundImage }) => $backgroundImage});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: inherit;
+  height: 100%;
+`;
+
+const FilmCardStyle = styled.li`
+  background-color: ${colors.SECONDARY};
+  border: 1px solid ${colors.BORDER};
+`;
+
+const ImagaCardStyle = styled.div`
+  border-right: 1px solid ${colors.TITLE_1};
+  width: inherit;
+  height: 100%;
+`;
+
+const ContentCardStyle = styled.div`
+  @media (max-width: 575px) {
+    padding: 5px;
+  }
+
+  @media (min-width: 576px) {
+    padding: 10px;
+  }
+
+  @media (min-width: 768px) {
+    padding: 10px;
+  }
+
+  @media (min-width: 992px) {
+    padding: 20px;
+  }
+
+  @media (min-width: 1200px) {
+    padding: 20px;
+  }
+`;
+
+const TitleCardStyle = styled.h3`
+  font-weight: bold;
+  font-size: ${sizes.XL2};
+  line-height: ${sizes.MD};
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: ${colors.TITLE_2};
+  margin-bottom: 10px;
+`;
+
+const DescriptionCardStyle = styled.p`
+  font-size: ${sizes.XS};
+  line-height: ${sizes.MD};
+  color: ${colors.TEXT};
+  margin-bottom: 10px;
+`;
+
+const LinkCardStyle = styled(Link)`
+  font-size: ${sizes.XS};
+  line-height: ${sizes.MD};
+  letter-spacing: 1px;
+  font-weight: normal;
+  text-transform: uppercase;
+  color: ${colors.TITLE_1};
+  text-decoration: none;
+  float: right;
+
+  &:hover {
+    font-weight: bold;
+    text-decoration: underline;
+  }
+`;
+
+function FilmCard({ $id, $title, $description }: FilmCardProps) {
   return (
-    <Row>
-      <Row $maxWidth="30%">
-        <Image $backgroundImage={getFilmImage($id)} />
+    <FilmCardStyle>
+      <Row>
+        <Row $maxWidth="30%">
+          <ImagaCardStyle>
+            <Image $backgroundImage={getFilmImage($id)} />
+          </ImagaCardStyle>
+        </Row>
+        <Row $maxWidth="70%">
+          <Column>
+            <ContentCardStyle>
+              <TitleCardStyle>{$title}</TitleCardStyle>
+              <DescriptionCardStyle>{$description}</DescriptionCardStyle>
+              <LinkCardStyle to={`/films/${$id}`}>VER MAIS</LinkCardStyle>
+            </ContentCardStyle>
+          </Column>
+        </Row>
       </Row>
-      <Row $maxWidth="70%">
-        <Column>
-          <h3>{$title}</h3>
-          <p>{$descriptiom}</p>
-          <Link to={`/films/${$id}`}>VER MAIS</Link>
-        </Column>
-      </Row>
-    </Row>
+    </FilmCardStyle>
   );
 }
 

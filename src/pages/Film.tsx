bab0aef6704304ba9acec8ed6015ef294be48a7d
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-13 00:17:49 -0300

    Refatoração da página para usar comp Page

--- src/pages/Film.tsx
+++ src/pages/Film.tsx
@@ -1,21 +1,22 @@
 import { Fragment } from "react";
 import { useParams } from "react-router-dom";
 import Body from "../components/Body";
+import FilmSinglePageCard from "../components/FilmSinglePageCard";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Page from "../components/Page";
+import Text from "../components/Text";
 import { FILM_MOCK_DATA } from "../constants";
+import { colors } from "../styles/themes/colors";
+import { sizes } from "../styles/themes/sizes";
 
 function Film() {
   const { id } = useParams<{ id: string }>();
 
   return (
-    <Fragment>
-      <Header />
-      <Body>
-        <h1>{id}</h1>
-      </Body>
-      <Footer />
-    </Fragment>
+    <Page>
+      <FilmSinglePageCard {...FILM_MOCK_DATA} />
+    </Page>
   );
 }
 

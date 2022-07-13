cb714b70a088badb6678c7eb8f36874d8ad26355
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-07-13 14:44:43 -0300

    função isAuthenticated usando localStorage

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,14 +1,18 @@
 import { useContext } from "react";
+import { USER } from "../constants";
+import useLocalStorage from "../hooks/useLocalStorage";
 import MyContext from "./MyContext";
 
-function isAuthenticated() {
-  const USER = {
-    USERNAME: "admin",
-    PASSWORD: "Admin123!",
-  };
+/* function isAuthenticated() {
   const { userName, password }: any = useContext(MyContext);
 
   return userName === USER.USERNAME && password === USER.PASSWORD;
-}
+} */
+const isAuthenticated = () => {
+  const username = JSON.parse(localStorage.getItem("username") || "");
+  const password = JSON.parse(localStorage.getItem("password") || "");
+
+  return username == USER.USERNAME && password == USER.PASSWORD;
+};
 
 export default isAuthenticated;
